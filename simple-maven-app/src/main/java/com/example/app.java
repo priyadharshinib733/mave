b0944f9ea620849cf08f3d9945ebd71cfed1f500
Priyadharshini B <priyadharshini.b2024b@vitstudent.ac.in>
2026-08-27 14:57:59 +0530

    Create app.java

---
+++ simple-maven-app/src/main/java/com/example/app.java
@@ -0,0 +1,14 @@
+package com.example
+  public class App{
+public static void main(String[] args){
+  System.out.println("Hello, Maven World!");
+  int a = 10;
+  it b = 20;
+  int sum = add(a,b);
+  
+  System.out.println("Sum of" + a + "and" + b + "is:"+sum);
+}
+    public static int add(int x, int y){
+      return x+y;
+    }
+  }
